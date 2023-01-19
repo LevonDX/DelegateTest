589b10a59ce1aba6a09827b0fb36cc3506276ca2
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-01-19 19:57:57 +0400

    Anonymus method

--- DelegateTest/Program.cs
+++ DelegateTest/Program.cs
@@ -1,23 +1,24 @@
 ﻿namespace DelegateTest
 {
-    internal class Program
+    class Program
     {
-        static bool IsEven(int item)
-        {
-            return item % 2 == 0;
-        }
-
-        static bool IsOdd(int item)
-        {
-            return item % 2 == 1;
-        }
-
         static void Main(string[] args)
         {
-            int[] x = { 1, 3, 2, 5, 0, 2, 3, 4, 9, 2, 3, 4, -6, 9, 0, 20 };
+            int[] array = { 1, 3, 2, 5, 0, 2, 3, 4,
+                9, 2, 3, 4, -6, 9, 0, 20, 7 };
 
-            x.GetElementsByCondition(IsOdd)
-                .ForEachElement(Console.WriteLine);
+            //array.GetElementsByCondition(IsDivThree);
+
+            int x = 10;
+
+            array.GetElementsByCondition(delegate (int item) // հայտարարում (definition)
+            {
+                x++;
+                return item % 5 == 0;
+
+            }).ForEachElement(Console.WriteLine);
+
+            Console.WriteLine($"x = {x}");
         }
     }
 }
